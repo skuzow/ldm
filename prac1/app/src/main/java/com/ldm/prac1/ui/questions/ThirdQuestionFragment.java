9875b64f1a8b1500021074f8783a6c460d622433
skuzow <alex@skuzow.net>
2024-10-09 17:57:48 +0200

    feat(prac1): questions fragments basis

---
+++ prac1/app/src/main/java/com/ldm/prac1/ui/questions/ThirdQuestionFragment.java
@@ -0,0 +1,25 @@
+package com.ldm.prac1.ui.questions;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ldm.prac1.R;
+
+public class ThirdQuestionFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_third_question, container, false);
+    }
+}
