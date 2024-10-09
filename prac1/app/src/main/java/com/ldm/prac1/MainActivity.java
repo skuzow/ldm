72fdf893c190ce1ae85c89c5bc9bafd8a8e6ef27
skuzow <alex@skuzow.net>
2024-10-09 19:40:12 +0200

    feat(prac1): navigation between questions

--- prac1/app/src/main/java/com/ldm/prac1/MainActivity.java
+++ prac1/app/src/main/java/com/ldm/prac1/MainActivity.java
@@ -1,8 +1,7 @@
 package com.ldm.prac1;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -12,9 +11,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.ldm.prac1.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +25,41 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_first_question, R.id.navigation_second_question, R.id.navigation_third_question)
+                R.id.navigation_first_question, R.id.navigation_second_question, R.id.navigation_third_question, R.id.navigation_fourth_question, R.id.navigation_fifth_question)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void onBack(View view) {
+        int currentNavDestinationId = Objects.requireNonNull(navController.getCurrentDestination()).getId();
+
+        if (currentNavDestinationId != R.id.navigation_first_question) {
+            navController.navigate(R.id.navigation_first_question);
+        }
+    }
+
+    public void onNext(View view) {
+        int currentNavDestinationId = Objects.requireNonNull(navController.getCurrentDestination()).getId();
+
+        if (currentNavDestinationId == R.id.navigation_first_question) {
+            navController.navigate(R.id.navigation_second_question);
+        }
+        else if (currentNavDestinationId == R.id.navigation_second_question) {
+            navController.navigate(R.id.navigation_third_question);
+        }
+        else if (currentNavDestinationId == R.id.navigation_third_question) {
+            navController.navigate(R.id.navigation_fourth_question);
+        }
+        else if (currentNavDestinationId == R.id.navigation_fourth_question) {
+            navController.navigate(R.id.navigation_fifth_question);
+        }
+        else if (currentNavDestinationId == R.id.navigation_fifth_question) {
+            // navigate to results activity
+        }
     }
 
 }
