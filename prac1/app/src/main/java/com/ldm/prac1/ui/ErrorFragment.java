6b32f617fbe701125775a29bb5007b43e2bd16a7
anna-trofimova <cobrana1000@gmail.com>
2024-10-14 20:19:57 +0200

    Added: radioButtons

--- prac1/app/src/main/java/com/ldm/prac1/ui/ErrorFragment.java
+++ prac1/app/src/main/java/com/ldm/prac1/ui/ErrorFragment.java
@@ -18,18 +18,6 @@ public class ErrorFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_error, container, false);
 
-        // Find the "Try Again" button
-        Button tryAgainButton = view.findViewById(R.id.button_try_again);
-
-        // Set a click listener on the button
-        tryAgainButton.setOnClickListener(v -> {
-            // Access MainActivity to call onNext
-            MainActivity mainActivity = (MainActivity) getActivity();
-            if (mainActivity != null) {
-                mainActivity.onNext(view);  // Call the onNext() method from MainActivity
-            }
-        });
-
         return view;
     }
 }
