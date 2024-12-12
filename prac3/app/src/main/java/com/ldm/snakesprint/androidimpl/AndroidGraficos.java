41274c67a92c55b5e3f124cc7157f5bb940e8abc
skuzow <alex@skuzow.net>
2024-12-12 11:24:22 +0100

    fix(prac3): draw rect android graphics

--- prac3/app/src/main/java/com/ldm/snakesprint/androidimpl/AndroidGraficos.java
+++ prac3/app/src/main/java/com/ldm/snakesprint/androidimpl/AndroidGraficos.java
@@ -106,7 +106,7 @@ public class AndroidGraficos implements Graficos {
     public void drawRect(int x, int y, int width, int height, int color) {
         paint.setColor(color);
         paint.setStyle(Style.FILL);
-        canvas.drawRect(x, y, x + width - 1, y + width - 1, paint);
+        canvas.drawRect(x, y, x + width - 1, y + height - 1, paint);
     }
 
     // Dibuja un Pixmap en una región específica del lienzo
