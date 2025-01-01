b7a4fb8da1028a381abfbb9d6ec69596d6369e7f
anna-trofimova <cobrana1000@gmail.com>
2025-01-01 18:57:37 +0100

    Changed:BD and graph

--- prac4/app/src/main/java/com/ldm/prac4/database/AppDatabase.java
+++ prac4/app/src/main/java/com/ldm/prac4/database/AppDatabase.java
@@ -1,11 +1,30 @@
 package com.ldm.prac4.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
 @Database(entities = {TaskEntity.class}, version = 1)
 @TypeConverters({DateTypeConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
+
+    private static volatile AppDatabase INSTANCE;
+
     public abstract TaskDao taskDao();
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "task_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
--- prac4/app/src/main/java/com/ldm/prac4/database/TaskDao.java
+++ prac4/app/src/main/java/com/ldm/prac4/database/TaskDao.java
@@ -1,5 +1,6 @@
 package com.ldm.prac4.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -7,13 +8,15 @@ import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
+import java.util.List;
+
 @Dao
 public interface TaskDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(TaskEntity task);
 
-    @Query("SELECT * FROM tasks WHERE id = :id")
-    TaskEntity findTaskById(int id);
+    @Query("SELECT * FROM tasks")  // Query to get all tasks
+    LiveData<List<TaskEntity>> getAllTasks();
 
     @Update
     void update(TaskEntity taskEntity);
